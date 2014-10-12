3d143a02446c1e6861741e54bbf4d59ae06cbbd4
Martin Owen <martin.j.owen@btinternet.com>
2014-10-12 17:20:30 +0100

    Replaced free-report form with call form for home page
    Signed-off-by: Martin Owen <martin.j.owen@btinternet.com>

--- sites/all/themes/mcorwd/layouts/no-sidebar/no-sidebar-layout.tpl.php
+++ sites/all/themes/mcorwd/layouts/no-sidebar/no-sidebar-layout.tpl.php
@@ -41,6 +41,7 @@ NO SIDEBAR
       <?php if ($action_links): ?>
         <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
+      <?php print render($page['hero']); ?>
       <?php print render($page['content']); ?>
       <?php print $feed_icons; ?>
     </div>    
--- sites/all/themes/mcorwd/layouts/right-sidebar/right-sidebar-layout.tpl.php
+++ sites/all/themes/mcorwd/layouts/right-sidebar/right-sidebar-layout.tpl.php
@@ -41,6 +41,7 @@ RIGHT SIDEBAR
       <?php if ($action_links): ?>
         <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
+      <?php print render($page['hero']); ?>
       <?php print render($page['content']); ?>
       <?php print render($page['postscript']); ?>
       <?php print $feed_icons; ?>
