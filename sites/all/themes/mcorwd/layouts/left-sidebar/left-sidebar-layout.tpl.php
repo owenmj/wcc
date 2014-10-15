e63e63e88aeec5d36687d83289fcb8d003dad48b
Martin Owen <martin.j.owen@btinternet.com>
2014-10-15 19:32:56 +0100

    Removed contact details for mobile
    Signed-off-by: Martin Owen <martin.j.owen@btinternet.com>

--- sites/all/themes/mcorwd/layouts/left-sidebar/left-sidebar-layout.tpl.php
+++ sites/all/themes/mcorwd/layouts/left-sidebar/left-sidebar-layout.tpl.php
@@ -22,7 +22,7 @@ LEFT SIDEBAR
       <?php print render($page['branding']); ?>
     </div>
 
-    <?php print render($page['header']); ?>
+    
     <?php print render($page['navigation']); ?>
     <?php print render($page['highlighted']); ?>
   </header>
@@ -41,6 +41,7 @@ LEFT SIDEBAR
       <?php if ($action_links): ?>
         <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
+      <?php print render($page['hero']); ?>
       <?php print render($page['content']); ?>
       <?php print $feed_icons; ?>
     </div>
